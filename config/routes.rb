2ead7b8e936a0ec2d4df4a4a1a09ec800df50526
Gaz Jones <jonesy5090@gmail.com>
2015-09-07 15:50:27 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
+  get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
+
+  #get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
